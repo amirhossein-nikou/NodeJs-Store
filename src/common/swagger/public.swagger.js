bf2e70553a6771b771fc1ad6e873a988f051a997
Amirhossein Nikoo <AmirhosseinNikoo2001@gmail.com>
2024-09-29 20:01:35 +0330

    public definition for swagger

---
+++ src/common/swagger/public.swagger.js
@@ -0,0 +1,16 @@
+/**
+ * @swagger
+ * definitions:
+ *      publicResult:
+ *          type: object
+ *          properties:
+ *              status:
+ *                  type: integer
+ *                  example: 20X
+ *              data:
+ *                  type: object
+ *                  properties:
+ *                      message:
+ *                          type: string
+ *                          example: result message
+ */
